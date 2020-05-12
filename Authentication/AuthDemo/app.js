b5128938ac6236ffe4303cfbaa8297def9782e78
0ishan0 <ishanbagchi4@gmail.com>
2020-05-12 20:56:09 +0000

    Add login page and its logic

--- Authentication/AuthDemo/app.js
+++ Authentication/AuthDemo/app.js
@@ -21,6 +21,7 @@ app.use(require("express-session")({
 app.use(passport.initialize());
 app.use(passport.session());
 
+passport.use(new localStrategy(User.authenticate()));
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
@@ -54,6 +55,20 @@ app.post("/register", function(req, res){
 			res.redirect("/secret");
 		});
 	});
+});
+
+// LOGIN ROUTES
+// Render login form
+app.get("/login", function(req, res){
+	res.render("login");
+});
+
+// Login logic
+// =>middleware
+app.post("/login",passport.authenticate("local", {
+	successRedirect: "/secret",
+	failureRedirect: "/register"
+}) , function(req, res){
 });
 
 app.listen(process.env.PORT || 3000, process.env.IP, function(){
