563e28f5d941a2fcb2aab8502adb7d329e98e6aa
0ishan0 <ishanbagchi4@gmail.com>
2020-05-12 20:27:18 +0000

    Add registration page

--- Authentication/AuthDemo/app.js
+++ Authentication/AuthDemo/app.js
@@ -12,6 +12,7 @@ mongoose.connect("mongodb://localhost:27017/auth_demo",
 
 var app = express();
 app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({extended: true}));
 app.use(require("express-session")({
 	secret:"Rusty is the best and cutest dog in the world",
 	resave: false,
@@ -23,12 +24,36 @@ app.use(passport.session());
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
+//===================
+// ROUTES
+//===================
+
 app.get("/", function(req, res){
 	res.render("home");
 });
 
 app.get("/secret", function(req, res){
 	res.render("secret");
+});
+
+// Auth Routes
+app.get("/register", function(req, res){
+	res.render("register");
+});
+
+// Handling user signup
+app.post("/register", function(req, res){
+	req.body.username
+	req.body.password
+	User.register(new User({username: req.body.username}), req.body.password, function(err, user){
+		if(err){
+			console.log(err);
+			return res.render("register");
+		}
+		passport.authenticate("local")(req, res, function(){
+			res.redirect("/secret");
+		});
+	});
 });
 
 app.listen(process.env.PORT || 3000, process.env.IP, function(){
