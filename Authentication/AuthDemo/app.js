96c63799e32ecc52aedb1f2e38bc1779a7d59846
0ishan0 <ishanbagchi4@gmail.com>
2020-05-12 21:15:37 +0000

    Finish Auth practice

--- Authentication/AuthDemo/app.js
+++ Authentication/AuthDemo/app.js
@@ -33,7 +33,7 @@ app.get("/", function(req, res){
 	res.render("home");
 });
 
-app.get("/secret", function(req, res){
+app.get("/secret", isLoggedIn, function(req, res){
 	res.render("secret");
 });
 
@@ -70,6 +70,18 @@ app.post("/login",passport.authenticate("local", {
 	failureRedirect: "/register"
 }) , function(req, res){
 });
+
+app.get("/logout", function(req, res){
+	req.logout();
+	res.redirect("/");
+});
+
+function isLoggedIn(req, res, next){
+	if(req.isAuthenticated()){
+		return next();
+	} 
+	res.redirect("/login");
+}
 
 app.listen(process.env.PORT || 3000, process.env.IP, function(){
 	console.log("Your app is running in 'https://yelpcamp-akkjg.run-ap-south1.goorm.io/'");
