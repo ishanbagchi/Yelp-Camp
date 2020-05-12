d6dcb587e6eca9ac9eacb403dd9a4949b024a608
0ishan0 <ishanbagchi4@gmail.com>
2020-05-12 19:54:27 +0000

    Set up user session

--- Authentication/AuthDemo/app.js
+++ Authentication/AuthDemo/app.js
@@ -1,5 +1,10 @@
-var express = require("express");
-var mongoose = require("mongoose");
+var express               = require("express"),
+	mongoose              = require("mongoose"),
+	passport              = require("passport"),
+	bodyParser            = require("body-parser"),
+	localStrategy         = require("passport-local"),
+	passportLocalMongoose = require("passport-local-mongoose"),
+	User                  = require("./models/user");
 
 mongoose.connect("mongodb://localhost:27017/auth_demo", 
 				 {useNewUrlParser: true, 
@@ -7,6 +12,16 @@ mongoose.connect("mongodb://localhost:27017/auth_demo",
 
 var app = express();
 app.set("view engine", "ejs");
+app.use(require("express-session")({
+	secret:"Rusty is the best and cutest dog in the world",
+	resave: false,
+	saveUninitialized: false
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
 app.get("/", function(req, res){
 	res.render("home");
