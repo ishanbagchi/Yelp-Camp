75d150bd102a5606ee966a685042bcd12c1b6efa
0ishan0 <ishanbagchi4@gmail.com>
2020-05-13 04:50:25 +0000

    Create the register page

--- app.js
+++ app.js
@@ -1,13 +1,15 @@
 const express = require("express");
-const app = express();
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 const passport = require("passport");
 const LocalStrategy = require("passport-local");
+
 const Campground = require("./models/campgrounds");
 const Comment = require("./models/comments");
 const User = require("./models/user");
 const seedDB = require("./seeds");
+
+const app = express();
 
 mongoose.connect("mongodb://localhost:27017/yelpcamp", 
 				 {useNewUrlParser: true, 
@@ -17,6 +19,17 @@ app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
 seedDB();
 
+// PASSPORT CONFIGURATION
+app.use(require("express-session")({
+	secret: "I love Panda, Panda loves sleeping!",
+	resave: false,
+	saveUninitialized: false
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
 app.get("/", function(req, res){
     res.render("landing");
@@ -108,6 +121,29 @@ app.post("/campgrounds/:id/comments", function(req, res){
 	// create new comment
 	// connect new comment to campground
 	// redirect to campgroundshow page
+});
+
+//====================
+// AUTH ROUTES
+//====================
+
+// show register form
+app.get("/register", function(req, res){
+	res.render("register");
+});
+
+// handel sign up logic
+app.post("/register", function(req, res){
+	var newUser = new User({username: req.body.username});
+	User.register(newUser, req.body.password, function(err, user){
+		if(err) {
+			console.log(err);
+			return res.render("register");
+		}
+		passport.authenticate("local")(req, res, function(){
+			res.redirect("/campgrounds");
+		});
+	});
 });
 
 app.listen(process.env.PORT || 3000, process.env.ID, function(){
