9a276a164213769b37fce7b340c810ca8be889a8
0ishan0 <ishanbagchi4@gmail.com>
2020-05-31 18:22:32 +0000

    add mongodb atlas

--- app.js
+++ app.js
@@ -16,9 +16,16 @@ const campgroundRoutes = require("./routes/campgrounds");
 const commentRoutes = require("./routes/comments");
 const indexRoutes = require("./routes/index");
 
-mongoose.connect("mongodb://localhost:27017/yelpcamp", 
-				 {useNewUrlParser: true, 
-				  useUnifiedTopology: true});
+mongoose.connect("mongodb+srv://ishanbagchi:ishaniman22@cluster0-9ffqc.gcp.mongodb.net/test?retryWrites=true&w=majority", {
+	useNewUrlParser: true, 
+	useCreateIndex: true,
+	useUnifiedTopology: true
+}).then(() => {
+	console.log('Connected to DB!');
+}).catch(err => {
+	console.log('ERROR:', err.message);
+});
+
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
